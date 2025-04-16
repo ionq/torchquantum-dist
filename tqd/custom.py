e97e503dee5844e00a0f78b57a0d1ebe50d30853
JMei <jmei@ionq.co>
2025-04-16 15:31:17 +0000

    invertible custom ops too

--- tqd/custom.py
+++ tqd/custom.py
@@ -3,7 +3,9 @@ from functools import partial
 from . import functional, matrices, operator
 
 def register_gate(name, mat):
+    name = name.lower()
     if name.lower() not in matrices.GATE_MAT_DICT:
-        matrices.GATE_MAT_DICT.update({name.lower(): mat})
-        globals()[name.lower()] = partial(functional.gate, name)
+        matrices.GATE_MAT_DICT.update({name: mat})
+        globals()[name] = partial(functional.gate, name)
+        globals()[f"{name}_inv"] = partial(functional.gate, name, inverse=True)
         globals()[name.upper()] = operator.OpFactory(name.lower(), globals())
