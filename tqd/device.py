b726e137727a06b89681fe4d024a0572195ae229
Oliver Knitter <oliver.knitter@ionq.co>
2025-08-18 01:45:09 +0000

    Finished writing new functionality of device.py, particularly the interchange and maybe_reshard functions.

--- tqd/device.py
+++ tqd/device.py
@@ -98,9 +98,8 @@ class DistributedQuantumDevice:
         self.reset_states()
 
     def reset_states(self):
-        self._wire_order = list(range(self.n_wires))
         # shard along last wire dimensions: assume that first computations use lower number wires
-        sharded_wires = (self._groupings[0][(self._groupings[1] == -1).nonzero().flatten()]).tolist()
+        sharded_wires = (self._groupings[:,(self._groupings[1] == -1)][0][-self.log2_devices:]).tolist()
         self._states = torch.zeros(self.local_shape, device=self.device)
         if self.global_rank == 0:
             self._states[(slice(None), ) + (0, ) * (self._states.ndim - 1)] = 1
@@ -120,81 +119,133 @@ class DistributedQuantumDevice:
         else:
             self._invertible_dummy = None
 
-    def interchange_qubits(self, wire1: int, wire2: int) -> bool:
+    def interchange_qubits(self, state: Union[DTensor, torch.Tensor], grouping: torch.Tensor, wire1: int, wire2: int) -> new_state: Union[DTensor, torch.Tensor], new_grouping: torch.Tensor:
         '''
-        Interchanges two qubits within the grouping. Does not reshard on its own and can only interchange sharded qubits with ungrouped qubits.
+        Interchanges two qubits within a given statevector. Does not reshard on its own and can only interchange sharded qubits with ungrouped qubits.
         Args:
+            state: Tensor or DTensor containing state information
+            grouping: grouping information for state
             wire1: first qubit index
             wire2: second qubit index
         Returns:
-            success: whether the interchange took place
+            new_state: Tensor or DTensor with interchanged qubits
+            new_grouping: new grouping information for new_state
         '''
-        wire_info = self._groupings[:,[wire1, wire2]]
-        if (wire_info[1] > -1).any():
+        wire_info = grouping[:,[wire1, wire2]]
+        if wire1 == wire2:
+            return state, grouping
+        elif (wire_info[1] > -1).any():
             if (wire_info[1] == -2).any(): # Cannot interchange sharded qubit with grouped unsharded qubit
-                return False
+                return state, grouping
             elif (wire_info[1] == -1).any(): # Exactly one wire is ungrouped
                 if wire_info[1,0] == -1: # Presume from now on that wire2 is the ungrouped wire
-                    return self.interchange_qubits(wire2, wire1)
+                    return self.interchange_qubits(state, grouping, wire2, wire1)
                 else:
                     lone_wire_idx = wire_info[0,1]
                     # Get index and size of wire1 group along with relative position of wire1
-                    grouped_wire_info = [wire_info[0,0].item(), wire_info[1,0].item(), self._groupings[0, self._groupings[0]==wire_info[0,0]].sum().item()]
+                    grouped_wire_info = [wire_info[0,0].item(), wire_info[1,0].item(), grouping[0, grouping[0]==wire_info[0,0]].sum().item()]
                     # grouped wires cannot be leftmost or rightmost dimensions; get index and size of left and right groups
-                    left_group_info = [grouped_wire_info[0] - 1, self._groupings[0, self._groupings[0]==(grouped_wire_info[0] - 1)].sum().item()]
-                    right_group_info = [grouped_wire_info[0] + 1, self._groupings[0, self._groupings[0]==(grouped_wire_info[0] + 1)].sum().item()]
-                    if lone_wire_idx in [left_group_info[0], right_group_info[0]]:
-                        all_ungrouped_idxs = self._groupings[0,[self._groupings[1] == -1]]
+                    left_group_info = [grouped_wire_info[0] - 1, grouping[0, grouping[0]==(grouped_wire_info[0] - 1)].sum().item()]
+                    right_group_info = [grouped_wire_info[0] + 1, grouping[0, grouping[0]==(grouped_wire_info[0] + 1)].sum().item()]
+ 
+                    need_left_interchange = (lone_wire_idx == left_group_info[0]) and (grouped_wire_info[1] > 0)
+                    need_right_interchange = (lone_wire_idx == right_group_info[0]) and (grouped_wire_info[1] < grouped_wire_info[2] - 1)
+                    need_interchange = need_left_interchange or need_right_interchange
+                    if need_interchange:
+                        all_ungrouped_idxs = grouping[0,[grouping[1] == -1]]
                         # Pick a new ungrouped qubit that isn't adjacent to wire1
                         helper_idx = all_ungrouped_idxs[((all_ungrouped_idxs != left_group_info[0])&(all_ungrouped_idxs != right_group_info[0]))][0]
-                        helper_qubit = (torch.nonzero(self._groupings[0] == helper_idx).flatten()[0]).int()
-                        op1 = self.interchange_qubits(wire1, helper_qubit)
-                        op2 = self.interchange_qubits(wire1, wire2)
-                        op3 = self.interchange_qubits(helper_qubit, wire2)
-                        return op1 & op2 & op3
+                        helper_qubit = (torch.nonzero(grouping[0] == helper_idx).flatten()[0]).int()
+                        new_state, new_grouping = self.interchange_qubits(state, grouping, wire1, helper_qubit)
+                        new_state, new_grouping = self.interchange_qubits(new_state, new_grouping, wire1, wire2)
+                        new_state, new_grouping = self.interchange_qubits(new_state, new_grouping, helper_qubit, wire2)
+                        return new_state, new_grouping
                     else:
-                        pass # TODO Case where one qubit is grouped, the other is ungrouped, and they are not adjacent in the grouping
+                        # Get temporary tensor shape
+                        state_shape = list(state.shape)
+                        temp_shape = state_shape
+                        temp_shape[grouped_wire_info[0]] = 2
+                        temp_shape[left_group_info[0]] = 2**(left_group_info[1] + grouped_wire_info[1])
+                        temp_shape[right_group_info[0]] = 2**(right_group_info[1] + grouped_wire_info[2] - 1 - grouped_wire_info[1])
+                        
+                        # Reshape to isolate grouped wire dimension, interchange wires, and then reshape back
+                        new_state = state.reshape(temp_shape)
+                        new_state,_ = self.interchange_dims(new_state, grouping, grouped_wire_info[0], lone_wire_idx)
+                        new_state = new_state.reshape(state_shape)
+                        
+                        new_grouping = grouping.detach().clone()
+                        new_grouping[:,wire1], new_grouping[:,wire2] = new_grouping[:,wire2], new_grouping[:,wire1]
+                        return new_state, new_grouping
             else: # If both wires are grouped, use ungrouped wire as medium of interchange
-                helper_qubit = (torch.nonzero(self._groupings[1] == -1).flatten()[0]).int()
-                op1 = self.interchange_qubits(wire2, helper_qubit)
-                op2 = self.interchange_qubits(wire1, wire2)
-                op3 = self.interchange_qubits(helper_qubit, wire1)
-                return op1 & op2 & op3
+                helper_qubit = (torch.nonzero(grouping[1] == -1).flatten()[0]).int()
+                new_state, new_grouping = self.interchange_qubits(state, grouping, wire2, helper_qubit)
+                new_state, new_grouping = self.interchange_qubits(new_state, new_grouping, wire1, wire2)
+                new_state, new_grouping = self.interchange_qubits(new_state, new_grouping, helper_qubit, wire1)
+                return new_state, new_grouping
         else:
             # Between ungrouped and sharded qubits, interchanging qubits is the same as interchanging dimensions
-            return self.interchange_dims(wire_info[0,0], wire_info[0,1])
+            return self.interchange_dims(state, grouping, wire_info[0,0], wire_info[0,1])
 
-    def interchange_dims(self, dim1: int, dim2: int) -> bool:
+    def interchange_dims(self, state: Union[DTensor, torch.Tensor], grouping: torch.Tensor, dim1: int, dim2: int) -> new_state: Union[DTensor, torch.Tensor], new_grouping = torch.Tensor:
         '''
-        Interchanges two tensor dimensions, either groups of qubits or ungrouped qubits, and updates self._groupings
+        Interchanges two tensor dimensions, either groups of qubits or ungrouped qubits, and updates grouping tensor
         Note that relative orders within dimensions (or designations of ungrouped or sharded qubits, remain unchanged
         Args:
+            state: Tensor or DTensor containing state info. Must containg self.num_dims dims
+            grouping: Tensor containing grouping info for state
             dim1: first dimension
             dim2: second dimension
         Results:
-            bool: confirming the interchange took place
+            new_state: new state with dimensions interchanged
+            new_grouping: new grouping with dimensions interchanged
         '''
         permute_list = list(range(self.num_dims))
         permute_list[dim1], permute_list[dim2] = dim2, dim1
-        self._states.permute(permute_list)
-        self._groupings[0, self._groupings[0]==dim1], self._groupings[0, self._groupings[0]==dim2] = dim2, dim1
-        return True
+        new_state = state.permute(permute_list)
+        new_grouping = grouping.detach().clone()
+        new_grouping[0,new_grouping[0]==dim1], new_grouping[0, new_grouping[0]==dim2] = dim2, dim1
+        return new_state, new_grouping
 
     def canonicalize(self):
-        self._states = self.states
-        self._invertible_dummy = self.invertible_dummy
-        self._wire_order = list(range(self.n_wires))
+        # self._states, self._groupings, self._invertible_dummy = self.reorder(self._states, self._groupings, self._invertible_dummy)
+        # First maybe_reshard final self.log2_devices qubits
+        sharded_wires = torch.nonzero(self._groupings[1] == -2).flatten()
+        wires_to_unshard = sharded_wires[sharded_wires < (self.n_wires - self.log2_devices)].tolist()
+        do_not_shard = list(range(self.n_wires - self.log2_devices))
+        for wire in wires_to_unshard:
+            self.maybe_reshard(wires=[wire], ignore=do_not_shard)
+        # Final qubits are all sharded (though possibly unordered); now obtain wire ordering
+        wire_order = torch.nonzero(self._groupings[0] == 1).flatten() # First wire group is always ungrouped
+        for i in range(2, self.num_dims - 1):
+            group_mask = self._groupings[0] == i
+            group_wires = torch.nonzero(group_mask).flatten()
+            if len(group_wires) > 1:
+                relative_order = self._groupings[1,group_mask].argsort()
+                group_wires = group_wires[relative_order]
+            wire_order = torch.cat((wire_order, group_wires))
+
+        # Now interchange qubits until they are all in the proper order
+        for i in range(self.n_wires):
+            if wire_order[i] == i:
+                continue
+            else:
+                if self._invertible_dummy is not None:
+                    self._invertible_dummy,_ = self.interchange_qubits(self._invertible_dummy, self._groupings, i, wire_order[i])
+                self._states, self._groupings = self.interchange_qubits(self._states, self._groupings, i, wire_order[i])
+                wire_order[wire_order == i], wire_order[i] = wire_order[i], i
     
     @property
     def states(self):
-        return self._states.permute((0, ) + tuple(1 + np.argsort(self._wire_order)) + (self.n_wires+1, ))
+        self.canonicalize()
+        return self._states.detach().clone()
 
     @property
     def invertible_dummy(self):
         if self._invertible_dummy is not None:
-            return self._invertible_dummy.permute((0, ) + tuple(1 + np.argsort(self._wire_order)) + (self.n_wires+1, ))
+            self.canonicalize()
+            return self._invertible_dummy.detach().clone()
 
-    def maybe_reshard(self, wires, inverse=False):
+    def maybe_reshard(self, wires: list[int], ignore: list[int], inverse: bool=False):
         """
         If the current sharding splits the statevector in the dimension that is acted upon by the
         gate, picks a new dimension to shard over and redistributes the statevector accordingly.
@@ -213,9 +264,10 @@ class DistributedQuantumDevice:
 
         As future work, we could make this a little stronger by examining the current connectivity.
 
-        Arguments:
-            `wires`: (`list` of `int`) indices of qubits that will be acted upon by the gate
-            `inverse`: when going in reverse direction for invertible backpropagation, we need to
+        Args
+            wires: (`list` of `int`) indices of qubits that will be acted upon by the gate. Presumed to contain at most two qubits
+            ignore: indices of qubits that will not be sharded
+            inverse: when going in reverse direction for invertible backpropagation, we need to
                 invert the dimension picking logic since the reversed ladder decreases in indices.
 
         Returns:
@@ -223,11 +275,11 @@ class DistributedQuantumDevice:
         """
         if self.world_sz <= 1:
             return
-        cur_sharded_qubits = {s_.dim-1 for s_ in self._states.placements}
+        cur_sharded_qubits = set(torch.nonzero(self._groupings[1] == -2).flatten().tolist())
         overlap = set(wires) & cur_sharded_qubits
         if overlap:  # only if wires affect sharded dimensions
             new_qubit_sharding = cur_sharded_qubits - overlap
-            usable_qubits = sorted(set(range(self._states.ndim - 2)) - (set(wires) | cur_sharded_qubits))
+            usable_qubits = sorted(set(range(self.n_wires)) - (set(wires) | cur_sharded_qubits | set(ignore)))
             # hardcode: 2qubit gates only
             min_wire = min(wires)
             max_wire = max(wires)
@@ -239,13 +291,27 @@ class DistributedQuantumDevice:
                     min_wire, max_wire = max_wire, min_wire + self.n_wires
             # this happens to be the same for inverse and not!
             best_usable_qubits = [q_ for q_ in usable_qubits if q_ > max_wire] + [q_ for q_ in usable_qubits if q_ < min_wire]
+            uncoupled_qubits = torch.nonzero(self._groupings[1] == -1).flatten().tolist()
+            switch_qubits = []
             for i in range(len(overlap)):
                 if inverse:
-                    new_qubit_sharding.add(best_usable_qubits[i])
+                    next_usable_qubit = best_usable_qubits[i]
                 else:
-                    new_qubit_sharding.add(best_usable_qubits[-1-i])
-            # all2all; add 1 for the batch dimension!
-            new_dim_sharding = [i + 1 for i, w in enumerate(self._wire_order) if w in new_qubit_sharding]
+                    next_usable_qubit = best_usable_qubits[-1-i]
+                # Move selected usable qubits into uncoupled dimensions (there are always at least 2 uncoupled qubits and at most 2 qubits selected)
+                if self._invertible_dummy is not None:
+                    self._invertible_dummy,_ = self.interchange_qubits(self._invertible_dummy, self._groupings, next_usable_qubit, uncoupled_qubits[i])
+                self._states, self._groupings = self.interchange_qubits(self._states, self._groupings, next_usable_qubit, uncoupled_qubits[i])
+                switch_qubits.append(next_usable_qubit)
+            for i in range(len(switch_qubits)):
+                # Interchange uncoupled selected qubits and sharded qubits in anticipation of resharding
+                if self._invertible_dummy is not None:
+                    self.invertible_dummy,_ = self.interchange_qubits(self._invertible_dummy, self.groupings, switch_qubits[i], list(overlap)[i])
+                self._states, self._groupings = self.interchange_qubits(self._states, self.groupings, switch_qubits[i], list(overlap)[i])
+                self._groupings[1,switch_qubits[i]], self._groupings[1,list(overlap)[i]] = -2, -1
+            new_qubit_sharding = new_qubit_sharding.union(switch_qubits)
+            # all2all
+            new_dim_sharding = [self._groupings[0, w] for w in new_qubit_sharding].sort()
             self._states = self._states.redistribute(self.device_mesh, placements=[Shard(d) for d in new_dim_sharding])
             if self._invertible_dummy is not None:
                 self._invertible_dummy = self._invertible_dummy.redistribute(self.device_mesh, placements=[Shard(d) for d in new_dim_sharding])
